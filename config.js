a66d1feeed6c556027dfe9d4defdf5702103bf3c
vplat981 <paarth.v@icloud.com>
2026-09-25 15:52:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 window.CRYSTONIA_SUPABASE_URL = 'https://pwaznnumgxdyxrdbhomm.supabase.co';
 window.CRYSTONIA_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3YXpubnVtZ3hkeXhyZGJob21tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyMjEzNTIsImV4cCI6MjEwNDc5NzM1Mn0.1c9nlSEdxTZYNMljMaoR89v2HfwDCwTTV6sN51uTNxg';
 // CyvaPay link for community support. Paste the full https:// link here to activate the support buttons.
-window.CRYSTONIA_CYVAPAY_URL = '';
+window.CRYSTONIA_CYVAPAY_URL = 'https://cyvathon.onrender.com/cyvapay/checkout/yKf46U5i6HNS';
